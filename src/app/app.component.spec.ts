09b64e1f8da0099fb83000214b5d613745d934c0
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-04-14 17:17:03 -0500

    setup inicial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mynewapp'`, () => {
+  it(`should have as title 'book-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mynewapp');
+    expect(app.title).toEqual('book-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mynewapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-list');
   });
 });
--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,11 +3,13 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { BookModule } from './book/book.module';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    BookModule,
   ],
   bootstrap: [AppComponent],
 })
