ab3bbc6117e706d227d08b7224e104e70c1bfe5e
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-04-14 17:31:29 -0500

    editorial y libro creado

---
+++ src/app/book/book-list/book-list.component.spec.ts
@@ -0,0 +1,29 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { BookListComponent } from './book-list.component';
+import { describe, beforeEach, it } from 'node:test';
+
+describe('BookListComponent', () => {
+  let component: BookListComponent;
+  let fixture: ComponentFixture<BookListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
