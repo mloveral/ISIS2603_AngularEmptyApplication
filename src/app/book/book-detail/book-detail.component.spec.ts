12e7b46b5ea7bebbf0bd1d0ea85e4427166ecf50
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-04-14 20:31:51 -0500

    cambios html y spec

--- src/app/book/book-detail/book-detail.component.spec.ts
+++ src/app/book/book-detail/book-detail.component.spec.ts
@@ -4,14 +4,20 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { BookDetailComponent } from './book-detail.component';
+import { Editorial } from '../../editorial/editorial';
+import { faker } from '@faker-js/faker';
+import { Author } from '../../author/author';
+import { BookDetail } from '../book-detail';
 
 describe('BookDetailComponent', () => {
   let component: BookDetailComponent;
   let fixture: ComponentFixture<BookDetailComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookDetailComponent ]
+      imports: [],
+      declarations: [ BookDetailComponent ],
     })
     .compileComponents();
   }));
@@ -19,10 +25,43 @@ describe('BookDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookDetailComponent);
     component = fixture.componentInstance;
+
+
+    const editorial = new Editorial(
+      faker.number.int(),
+      faker.lorem.sentence()
+    );
+
+
+    const authors: Author[] = [];
+    for (let i = 0; i < 3; i++) {
+      const author = new Author (
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      );
+      authors.push(author);
+    }
+
+
+    component.bookDetail= new BookDetail(
+      faker.number.int(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.image.url(),
+      faker.date.past(),
+      editorial,
+      authors,
+      []
+    );
+
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
