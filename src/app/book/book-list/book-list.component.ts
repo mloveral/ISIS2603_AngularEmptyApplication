ba4678a6b61b9176a560205014dce3bca1a3fd74
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-04-14 19:22:55 -0500

    cypress added

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -5,7 +5,8 @@ import { BookService } from '../book.service';
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+  styleUrls: ['./book-list.component.css'],
+  standalone: false
 })
 export class BookListComponent implements OnInit {
 
